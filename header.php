7e98e78b00b33243ebde7edbebedf5fb5e68ef7b
hama0115 <oh.akibee1231@gmail.com>
2025-03-31 21:03:14 +0900

    リセットCSSを追加

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
   <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?> >
+    <?php wp_body_open(); ?>
     <header class="header">
       <div class="header-inner">
         <a href="<?php echo esc_url(home_url('/')); ?>" class="header-logo"><?php bloginfo('name'); ?></a>
