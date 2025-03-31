1b66b3d69463d721a0d2791a704c2fd5b9aee81c
hama0115 <oh.akibee1231@gmail.com>
2025-03-31 20:48:22 +0900

    ファーストコミット。各テンプレートをとりあえず用意したところ

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+    <main class="main">    
+      <div class="content-wrapper">
+        <div class="content"></div>
+      </div>  
+    </main>
+
+<?php get_footer(); ?>
