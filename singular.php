6a89dd3bcc28d78a3fc422b359d79b4891b9eabe
hama0115 <oh.akibee1231@gmail.com>
2025-04-03 07:42:56 +0900

    メインコンテンツのPCサイズのスタイルを調整

--- singular.php
+++ singular.php
@@ -1,17 +1,14 @@
 <?php get_header(); ?>
 
-    <main class="main">    
-      <div class="content-wrapper">
-        <div class="content">
-
+  <main class="main">
+    <div class="container">      
+      <div class="main-content">
+        <div class="content-inner">
           <?php if( have_posts() ): while( have_posts() ) : the_post(); ?>
-          
           <article id="post-<?php the_ID(); ?>" <?php post_class('single-article'); ?>>
             <div class="title-area">
-
               <h1 class="article-title"><?php the_title(); ?></h1>
               <time class="entry-date"><?php echo get_the_date(); ?></time>
-
               <?php /* カテゴリーをリンクなしで表示 */
               $cats = get_the_category();
               if($cats):
@@ -22,50 +19,38 @@
                 <?php endforeach; ?>
                 </ul>
               <?php endif; ?>
-
               <?php get_template_part( 'tmp/breadcrumb' ); ?>
-
             </div>
-
             <?php if(has_post_thumbnail()): ?>
               <div class="eyecatch-area"><?php the_post_thumbnail(); ?></div>
             <?php endif; ?>
-
             <div class="article-content">
               <?php the_content(); ?>
             </div>
-
             <div class="page-link"><!-- 前後の記事がある場合はリンクを表示 -->
               <?php if( get_previous_post() ): ?>
                 <p class="prev-page-link"><?php previous_post_link('%link', '%title'); //関数デフォルトの記号を非表示 ?></p>
               <?php endif; ?>
-              
               <?php if( get_next_post() ): ?>
                 <p class="next-page-link"><?php next_post_link('%link', '%title'); //関数デフォルトの記号を非表示 ?></p>
               <?php endif; ?>
             </div>
           </article>
-
           <?php endwhile; else: ?>
-
-          <?php endif; ?>          
-          
+          <?php endif; ?>
           <section class="latest-pages">
             <h2 class="latest-pages-title">最新の記事</h2>
             <?php
             if ( is_singular() ) {
               $postid = get_the_ID();
               $authorid = get_the_author_meta( 'ID' );
-
               $args = [
                 'posts_per_page' => 5,
                 'author' => $authorid,
                 'orderby' => 'date',
                 'exclude' => $postid,
               ];
-
               $myposts = get_posts( $args );
-              
               if ( $myposts ) :
                 echo '<ul>';
                 foreach ( $myposts as $post ) :
@@ -83,6 +68,12 @@
           </section>
         </div>
       </div>
-    </main>
+      <aside class="sidebar">
+        <div class="sidebar-inner">
+          <?php get_sidebar(); ?>
+        </div>
+      </aside>      
+    </div>
+  </main>
     
 <?php get_footer(); ?>
