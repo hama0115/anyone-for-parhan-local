0ac337439b51f6a9bbe2284e7e8eea1e60cd470c
hama0115 <oh.akibee1231@gmail.com>
2025-04-03 18:25:52 +0900

    アーカイブページのカードのスタイルを調整

--- front-page.php
+++ front-page.php
@@ -12,28 +12,30 @@
                 <li>
                   <a href="<?php the_permalink(); ?>" >
             
-                  <div class="thumbnail-area">
-                    <?php
-                    if(has_post_thumbnail()):
-                      the_post_thumbnail('full');
-                    else: ?>
-                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sample-thumbnail.JPG" alt="ダミーのサムネイル">
-                    <?php endif; ?>
-                  </div>
-            
-                    <div class="text">
-                      <time class="entry-date"><?php echo get_the_date(); ?></time>
-                      <p class="article-title"><?php the_title(); ?></p>
-                      <?php /* カテゴリーをリンクなしで表示 */
-                      $cats = get_the_category();
-                      if($cats):
-                      ?>
-                        <ul class="category-list">
-                        <?php foreach($cats as $cat): ?>
-                          <li class="article-category"><?php echo $cat->name; ?></li>
-                        <?php endforeach; ?>
-                        </ul>
+                    <div class="thumbnail-area">
+                      <?php
+                      if(has_post_thumbnail()):
+                        the_post_thumbnail('full');
+                      else: ?>
+                      <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/sample-thumbnail.JPG" alt="ダミーのサムネイル">
                       <?php endif; ?>
+                    </div>
+            
+                    <div class="text">                      
+                      <p class="article-title"><?php the_title(); ?></p>
+                      <div class="information">
+                        <time class="entry-date"><?php echo get_the_date(); ?></time>
+                        <?php /* カテゴリーをリンクなしで表示 */
+                        $cats = get_the_category();
+                        if($cats):
+                        ?>
+                          <ul class="category-list">
+                          <?php foreach($cats as $cat): ?>
+                            <li class="article-category"><?php echo $cat->name; ?></li>
+                          <?php endforeach; ?>
+                          </ul>
+                        <?php endif; ?>
+                      </div>
                     </div>
                   </a>
                 </li>
