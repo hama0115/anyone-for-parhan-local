c7a328be3ba94066da7eee14ec823d13953f8e31
hama0115 <oh.akibee1231@gmail.com>
2025-04-03 19:56:12 +0900

    サイドバーに「最新の記事」を追加

--- sidebar.php
+++ sidebar.php
@@ -2,6 +2,34 @@
 <div class="search-block">
   <?php get_search_form(); ?>
 </div>
+<!-- 最新の記事エリア -->
+<section class="latest-pages">
+  <h2 class="latest-pages-title">最新の記事</h2>
+  <?php
+    $postid = get_the_ID();
+    $authorid = get_the_author_meta( 'ID' );
+    $args = [
+      'posts_per_page' => 5,
+      'author' => $authorid,
+      'orderby' => 'date',
+      'exclude' => $postid,
+    ];
+    $myposts = get_posts( $args );
+    if ( $myposts ) :
+      echo '<ul>';
+      foreach ( $myposts as $post ) :
+        setup_postdata( $post ) ?>
+        <li>
+          <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+        </li>
+      <?php endforeach;
+      wp_reset_postdata();
+      echo '</ul>';
+    else :
+      echo '記事はありません。';
+    endif ;
+  ?>
+</section>
 <!-- アーカイブエリア -->
 <div class="archive-block">
   <h3>過去のアーカイブ</h3>
--- singular.php
+++ singular.php
@@ -38,34 +38,6 @@
           </article>
           <?php endwhile; else: ?>
           <?php endif; ?>
-          <section class="latest-pages">
-            <h2 class="latest-pages-title">最新の記事</h2>
-            <?php
-            if ( is_singular() ) {
-              $postid = get_the_ID();
-              $authorid = get_the_author_meta( 'ID' );
-              $args = [
-                'posts_per_page' => 5,
-                'author' => $authorid,
-                'orderby' => 'date',
-                'exclude' => $postid,
-              ];
-              $myposts = get_posts( $args );
-              if ( $myposts ) :
-                echo '<ul>';
-                foreach ( $myposts as $post ) :
-                  setup_postdata( $post ) ?>
-                  <li>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                  </li>
-                <?php endforeach;
-                wp_reset_postdata();
-                echo '</ul>';
-              else :
-                echo '記事はありません。';
-              endif ;
-            }?>
-          </section>
         </div>
       </div>
       <aside class="sidebar">
