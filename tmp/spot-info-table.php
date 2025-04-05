3d32faebb5a84ee3f6c600afd40f7c3fc1e8b62c
hama0115 <oh.akibee1231@gmail.com>
2025-04-06 03:33:40 +0900

    スポット情報をACFのチェックボックスで選べるように設定

---
+++ tmp/spot-info-table.php
@@ -0,0 +1,13 @@
+<!-- ACF「「スポット情報」を表示 -->
+<?php
+$selected_icons = get_field('spot-information');
+
+if ($selected_icons):
+  echo '<div class="spot-list">';
+  foreach ($selected_icons as $icon) {
+    $icon_url = get_stylesheet_directory_uri() . '/assets/img/' . $icon . '.png';
+    echo '<img src="' . esc_url($icon_url) . '" alt="' . esc_attr($icon) . '" class="spot-icon">';
+  }
+  echo '</div>';
+endif;
+?>
