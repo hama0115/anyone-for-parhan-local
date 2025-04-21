61791cb0e918d093a67b10d8bb3480e29cd64a08
hama0115 <oh.akibee1231@gmail.com>
2025-04-21 19:49:52 +0900

    アーカイブエリアのウィジェットをテンプレート化

--- sidebar.php
+++ sidebar.php
@@ -3,9 +3,4 @@
 <!-- 最新の記事エリア -->
 <?php get_template_part ( 'tmp/latest-pages' ) ?>
 <!-- アーカイブエリア -->
-<div class="archive-block">
-  <h3>過去のアーカイブ</h3>
-  <ul class="yearly-archive">
-    <?php wp_get_archives('type=yearly'); ?>
-  </ul>
-</div>
+<?php get_template_part ( 'tmp/archive-area' ) ?>
---
+++ tmp/archive-area.php
@@ -0,0 +1,7 @@
+<!-- アーカイブエリア -->
+<div class="archive-block">
+  <h3>過去のアーカイブ</h3>
+  <ul class="yearly-archive">
+    <?php wp_get_archives('type=yearly'); ?>
+  </ul>
+</div>
