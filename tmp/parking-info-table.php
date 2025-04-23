6c0f1613ae5bc7811969db60878697009668fa66
hama0115 <oh.akibee1231@gmail.com>
2025-04-23 12:50:14 +0900

    スポットアイコンをサイズ調整した

--- tmp/parking-info-table.php
+++ tmp/parking-info-table.php
@@ -26,7 +26,7 @@
         if ($selected_icons):
           echo '<div class="spot-list">';
           foreach ($selected_icons as $icon) {
-            $icon_url = get_stylesheet_directory_uri() . '/assets/img/' . $icon . '.png';
+            $icon_url = get_stylesheet_directory_uri() . '/assets/img/spot-icon/' . $icon . '.jpeg';
             echo '<img src="' . esc_url($icon_url) . '" alt="' . esc_attr($icon) . '" class="spot-icon">';
           }
           echo '</div>';
