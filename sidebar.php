27aaedba71c0c2e4b9df2f17b7625fc83ed6ab79
hama0115 <oh.akibee1231@gmail.com>
2025-04-29 19:29:28 +0900

    git add .

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
 <!-- リンク集エリア -->
-<?php get_template_part ( 'tmp/link' ) ?>
+<?php get_template_part ( 'tmp/post-it' ) ?>
 <!-- アーカイブエリア -->
 <?php get_template_part ( 'tmp/archive-area' ) ?>
---
+++ tmp/post-it.php
@@ -0,0 +1,5 @@
+<div class="post-it">
+  <div class="post-it-area">
+    <a href="https://luup.sc/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/luup.jpg" alt="luupの付箋"></a>
+  </div>
+</div>
