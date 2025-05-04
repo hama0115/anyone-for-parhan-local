bd0589b443044cfd22e00e09453f93c3d0d3b645
hama0115 <oh.akibee1231@gmail.com>
2025-05-04 10:10:44 +0900

    スライドショーの基本機能を実装

--- assets/js/swiper-slide.js
+++ assets/js/swiper-slide.js
@@ -7,5 +7,12 @@ const swiper1 = new Swiper(".swiper" , {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev"
+  },
+  //ループ
+  loop: true,
+  //オートプレイ
+  autoplay: {
+  delay: 3000, //3秒ごと
+  disableOnIntersection: false //ユーザーの操作があっても自動再生を継続
   }
 });
