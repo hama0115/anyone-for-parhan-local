add566c24bc425d4622e50cfdd214ade2902efe7
hama0115 <oh.akibee1231@gmail.com>
2025-05-06 13:21:24 +0900

    サブメニューをとりあえず表示させた

---
+++ test.php
@@ -0,0 +1,20 @@
+<nav class="site-menu">
+  <ul id="menu-%e3%83%a1%e3%83%8b%e3%83%a5%e3%83%bc%e3%83%90%e3%83%bc" class="menu"><li id="menu-item-89" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-has-children menu-item-89"><a href="http://parhan.local/category/area/">エリア</a>
+  <ul class="sub-menu">
+    <li id="menu-item-246" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-246"><a href="http://parhan.local/category/area/chuo-city/">中央区</a>
+    </li>
+    <li id="menu-item-247" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-247"><a href="http://parhan.local/category/area/chiyoda-city/">千代田区</a>
+    </li>
+    <li id="menu-item-248" class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-248"><a href="http://parhan.local/category/area/koto-city/">江東区</a>
+    </li>
+  </ul>
+</li>
+<li id="menu-item-243" class="menu-item menu-item-type-post_type_archive menu-item-object-restaurant menu-item-has-children menu-item-243"><a href="http://parhan.local/restaurant/">飲食店一覧</a>
+<ul class="sub-menu">
+	<li id="menu-item-249" class="menu-item menu-item-type-taxonomy menu-item-object-genre menu-item-249"><a href="http://parhan.local/genre/asia-cuisine/">アジア料理</a></li>
+	<li id="menu-item-250" class="menu-item menu-item-type-taxonomy menu-item-object-genre menu-item-250"><a href="http://parhan.local/genre/jp-food/">和食</a></li>
+	<li id="menu-item-251" class="menu-item menu-item-type-taxonomy menu-item-object-genre menu-item-251"><a href="http://parhan.local/genre/wes-food/">洋食</a></li>
+</ul>
+</li>
+</ul>
+</nav>
