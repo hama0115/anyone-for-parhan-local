c14b4bf33c36e9ad09e9d5d55f831100f3cbf332
hama0115 <oh.akibee1231@gmail.com>
2025-05-28 03:05:53 +0900

    「飲食店情報ブロック」全体のクラスを追加。また背景色を追加

--- acf-blocks/restaurant-info/restaurant-info.php
+++ acf-blocks/restaurant-info/restaurant-info.php
@@ -1,55 +1,57 @@
 <!-- ACF「飲食店情報」を表示 -->
 <?php if(have_rows('restaurant-info')): ?>
   <?php while(have_rows('restaurant-info')): the_row(); ?>
-    <?php if(get_sub_field('restaurant-name')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
-    <p class="restaurant-name"><?php the_sub_field('restaurant-name'); ?></p>
-    <?php endif; ?>
-    <div class="restaurant-info-container">
-      <?php //対象のサブフィールド(画像)が存在する場合に出力
-      $image = get_sub_field('restaurant-photo');
-      if(!empty($image)):
-      ?>
-      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+    <div class="restaurant-block">
+      <?php if(get_sub_field('restaurant-name')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
+      <p class="restaurant-name"><?php the_sub_field('restaurant-name'); ?></p>
       <?php endif; ?>
-      <?php //対象のサブフィールド(画像)が存在する場合に出力
-      $image = get_sub_field('menu-photo');
-      if(!empty($image)):
-      ?>
-      <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-      <?php endif; ?>
-      <?php //対象のサブフィールド(googleマップ)が存在する場合に出力
-      $location = get_sub_field('area-map');
-      if( $location ): ?>
-        <div class="acf-map" data-zoom="16">
-          <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+      <div class="restaurant-info-container">
+        <?php //対象のサブフィールド(画像)が存在する場合に出力
+        $image = get_sub_field('restaurant-photo');
+        if(!empty($image)):
+        ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+        <?php endif; ?>
+        <?php //対象のサブフィールド(画像)が存在する場合に出力
+        $image = get_sub_field('menu-photo');
+        if(!empty($image)):
+        ?>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+        <?php endif; ?>
+        <?php //対象のサブフィールド(googleマップ)が存在する場合に出力
+        $location = get_sub_field('area-map');
+        if( $location ): ?>
+          <div class="acf-map" data-zoom="16">
+            <div class="marker" data-lat="<?php echo esc_attr($location['lat']); ?>" data-lng="<?php echo esc_attr($location['lng']); ?>"></div>
+          </div>
+        <?php endif; ?>
+        <div class="restaurant-text-container">
+          <?php //対象のサブフィールド(リンク)が存在する場合に出力
+          $hp = get_sub_field('hp');
+          if (is_array($hp) && !empty($hp['url']) && !empty($hp['title'])): ?>
+            <a href="<?php echo esc_url($hp['url']); ?>">
+              <p>【HP】<?php echo esc_html($hp['title']); ?></p>
+            </a>
+          <?php endif; ?>
+          <?php if(get_sub_field('takeout')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
+          <p>【テイクアウト】<?php the_sub_field('takeout'); ?></p>
+          <?php endif; ?>
+          <?php if(get_sub_field('order')): ?>
+            <p>【予約】<?php the_sub_field('order'); ?></p>
+          <?php endif; ?>
+          <?php if(get_sub_field('opening-hours')): ?>
+            <p>【営業時間】<?php the_sub_field('opening-hours'); ?></p>
+          <?php endif; ?>
         </div>
-      <?php endif; ?>
-      <div class="restaurant-text-container">
-        <?php //対象のサブフィールド(リンク)が存在する場合に出力
-        $hp = get_sub_field('hp');
-        if (is_array($hp) && !empty($hp['url']) && !empty($hp['title'])): ?>
-          <a href="<?php echo esc_url($hp['url']); ?>">
-            <p>【HP】<?php echo esc_html($hp['title']); ?></p>
-          </a>
-        <?php endif; ?>
-        <?php if(get_sub_field('takeout')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
-        <p>【テイクアウト】<?php the_sub_field('takeout'); ?></p>
-        <?php endif; ?>
-        <?php if(get_sub_field('order')): ?>
-          <p>【予約】<?php the_sub_field('order'); ?></p>
-        <?php endif; ?>
-        <?php if(get_sub_field('opening-hours')): ?>
-          <p>【営業時間】<?php the_sub_field('opening-hours'); ?></p>
-        <?php endif; ?>
-      </div>      
-    </div>
-    <div class="restaurant-desc">
-      <?php if(get_sub_field('desc')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
-        <dl>
-          <dt>【飲食店の概要】</dt>
-          <dd><?php the_sub_field('desc'); ?></dd>
-        </dl>
-      <?php endif; ?>
       </div>
+      <div class="restaurant-desc">
+        <?php if(get_sub_field('desc')): //対象のサブフィールド(テキスト)が存在する場合に出力 ?>
+          <dl>
+            <dt>【飲食店の概要】</dt>
+            <dd><?php the_sub_field('desc'); ?></dd>
+          </dl>
+        <?php endif; ?>
+      </div>
+    </div>
   <?php endwhile; ?>
 <?php endif; ?>
