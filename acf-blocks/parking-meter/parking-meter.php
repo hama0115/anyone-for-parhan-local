cc9594ac9ccf2005bf69eb583f21c73ffb8bcb4f
hama0115 <oh.akibee1231@gmail.com>
2025-06-17 20:00:26 +0900

    「パーキング情報」の「周辺のスポット情報」をpngに変更。また「図書館」を追加

--- acf-blocks/parking-meter/parking-meter.php
+++ acf-blocks/parking-meter/parking-meter.php
@@ -27,7 +27,7 @@
         if ($selected_icons):
           echo '<div class="spot-list">';
           foreach ($selected_icons as $icon) {
-            $icon_url = get_stylesheet_directory_uri() . '/assets/img/spot-icon/' . $icon . '.jpeg';
+            $icon_url = get_stylesheet_directory_uri() . '/assets/img/spot-icon/' . $icon . '.png';
             echo '<img src="' . esc_url($icon_url) . '" alt="' . esc_attr($icon) . '" class="spot-icon">';
           }
           echo '</div>';
