cb9e38eebcb8de1e772e8ddc964a2accffb80be7
hama0115 <oh.akibee1231@gmail.com>
2025-06-24 21:09:07 +0900

    付箋の追加

--- sidebar.php
+++ sidebar.php
@@ -2,3 +2,9 @@
 <?php get_template_part ( 'tmp/post-it' ) ?>
 <!-- アーカイブエリア -->
 <?php get_template_part ( 'tmp/archive-area' ) ?>
+<!-- 目次エリア(「LuckyWP Table of Contents」使用) -->
+<?php
+ if ( function_exists('lwptoc') ) {
+  echo lwptoc();
+ }
+?>
--- tmp/post-it.php
+++ tmp/post-it.php
@@ -2,4 +2,20 @@
   <div class="post-it-area">
     <a href="https://luup.sc/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/luup.jpg" alt="luupの付箋"></a>
   </div>
+  <div class="post-it-area">
+    <a href="https://share.timescar.jp/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/timescarshare.png" alt="タイムズカーシェアの付箋"></a>
+  </div>
+  <div class="post-it-area">
+    <a href="https://docomo-cycle.jp/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/docomobikeshare.png" alt="ドコモバイクシェアの付箋"></a>
+  </div>
+  <div class="post-it-area">
+    <a href="https://www.akippa.com/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/akippa.png" alt="アキッパの付箋"></a>
+  </div>
+  <div class="post-it-area">
+    <a href="https://www.ubereats.com/jp/feed?diningMode=PICKUP&lat=35.6827685&lng=139.7989115&referrer=https%3A%2F%2Fwww.google.com%2F&so=https%3A%2F%2Fwww.ubereats.com%2Fjp%3Fsrsltid%3DAfmBOoqAWZcYSS7XYiEq1wrusG7jc3S8JwZg9BK8Ta7wXc6WhZPQlT09
+"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/ubereats.png" alt="ウーバーイーツの付箋"></a>
+  </div>
+  <div class="post-it-area">
+    <a href="https://www.tablecheck.com/ja/japan?availability_days_limit=7&availability_format=date&date_min=2025-06-11&date_max=2025-08-10&geo_distance=1km&service_mode=dining&sort_by=distance&venue_type=all&geo_latitude=35.6823&geo_longitude=139.7987&auto_geolocate=false"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/post-it/tablecheck.png" alt="テーブルチェックの付箋"></a>
+  </div>
 </div>
