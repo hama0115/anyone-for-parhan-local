8df7b4109ea5be5d464096ca4c1c9c5b29e22b80
hama0115 <oh.akibee1231@gmail.com>
2025-08-25 05:27:11 +0900

    画像をマーカーとして表示してみた

--- assets/js/custommap.js
+++ assets/js/custommap.js
@@ -4,7 +4,7 @@ function initMap() {
   //清澄白河の位置を取得
   const map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 35.6792227, lng: 139.80513297370942 },
-    zoom: 15,
+    zoom: 16,
     mapId: '42e4a02c987a2573d8e4be12'
   });
 
@@ -14,7 +14,7 @@ function initMap() {
   });
   const marker1 = new google.maps.marker.AdvancedMarkerElement({
     map: map,
-    position: { lat: 35.67835555735313, lng: 139.80513297370942 },
+    position: { lat: 35.67776094983446, lng: 139.80518018209077 },
     title: "Allpress Espresso Tokyo Roastery & Cafe",
     content: pinBackground.element,
   });
@@ -31,6 +31,20 @@ function initMap() {
     position: { lat: 35.67973018612079, lng: 139.80513297370942 },
     map: map,
     title: "洋食屋POND",
+  });
+
+  //築地やまの のマーカー
+  const yamanoImg = document.createElement('img');
+  yamanoImg.src = '/wp-content/themes/anyone-for-parhan/assets/img/yamano.jpeg';
+  yamanoImg.style.width = '40px';
+  yamanoImg.style.height = '40px';
+  yamanoImg.style.border = 'solid 3px #fff';
+  yamanoImg.style.borderRadius = '50%';
+  const yamanoMarkerView = new google.maps.marker.AdvancedMarkerElement({
+    map: map,
+    position: { lat: 35.679227687817, lng:139.80105802495973 },
+    content: yamanoImg,
+    title: '築地やまの',
   });
 }
 
@@ -51,5 +65,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //メモ
 //(清澄白河の経緯) 35.6792227, 139.8019834
-//(「オールプレスエスプレッソ」の経緯) 35.67835555735313, 139.80513297370942
+//(「オールプレスエスプレッソ」の経緯) 35.67776094983446, 139.80518018209077
 //(「洋食屋POND」の経緯)35.67973018612079, 139.80174907972332
+//(「築地やまの」の経緯) 35.679227687817, 139.80105802495973
